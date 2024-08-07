94cc1c1b4a0b07ae161311bb4322c053dc33b791
Karkowin <karkowin@gmail.com>
2024-08-07 18:18:33 +0200

    Fixing mail JSON parsing

--- tools/mail.sh
+++ tools/mail.sh
@@ -7,34 +7,22 @@ send_email() {
   local BACKUP_NAME=$1
   local ERROR_MESSAGE=$2
 
-  # Create JSON payload
-  local PAYLOAD=$(cat <<EOF
-{
-  "sender": {
-    "email": "$EMAIL_FROM",
-    "name": "$EMAIL_FROM_NAME"
-  },
-  "subject": "$EMAIL_SUBJECT",
-  "htmlContent": "<p>Backup <strong>$BACKUP_NAME</strong> failed with error:</p></br><p>$ERROR_MESSAGE</p>",
-  "messageVersions": [
-    {
-      "to": [
-        {
-          "email": "$EMAIL_TO",
-          "name": "$EMAIL_TO_NAME"
-        }
-      ]
-    }
-  ]
-}
-EOF
-)
+  # Create JSON payload using jq
+  local PAYLOAD=$(jq -n \
+    --arg email_from "$EMAIL_FROM" \
+    --arg email_from_name "$EMAIL_FROM_NAME" \
+    --arg email_subject "$EMAIL_SUBJECT" \
+    --arg email_to "$EMAIL_TO" \
+    --arg email_to_name "$EMAIL_TO_NAME" \
+    --arg backup_name "$BACKUP_NAME" \
+    --arg error_message "$ERROR_MESSAGE" \
+    '{sender: {email: $email_from, name: $email_from_name}, subject: $email_subject, htmlContent: "<p>Backup <strong>\($backup_name)</strong> failed with error:</p></br><p>\($error_message)</p>", messageVersions: [{to: [{email: $email_to, name: $email_to_name}]}]}')
 
   # Send email
   curl --request POST \
-    --url $BREVO_API_URL \
+    --url "$BREVO_API_URL" \
     --header 'accept: application/json' \
-    --header 'api-key: '$BREVO_API_KEY \
+    --header "api-key: $BREVO_API_KEY" \
     --header 'content-type: application/json' \
     --data "$PAYLOAD"
 }
@@ -48,4 +36,4 @@ fi
 # Call send_email function with arguments
 send_email "$1" "$2"
 
-# ./script.sh "Daily Backup" "Error: unable to connect to database"
+# ./tools/mail.sh "Daily Backup" "Error: unable to connect to database"
